3f780efb9e971bd07c7e25c596372a5f19855c1e
Assis Moraes <assis_fest@live.it>
2017-02-22 00:54:12 -0300

    Aula 09 - Criando Migrations ACL FOR LARGE SYSTEMS

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -263,8 +263,10 @@ class ComposerStaticInit1608685856526c3ddf9f298e4e6e8454
     );
 
     public static $classMap = array (
-        'CreateNoticesTable' => __DIR__ . '/../..' . '/database/migrations/2017_02_21_124315_create_notices_table.php',
         'CreatePasswordResetsTable' => __DIR__ . '/../..' . '/database/migrations/2014_10_12_100000_create_password_resets_table.php',
+        'CreatePermissionsTable' => __DIR__ . '/../..' . '/database/migrations/2017_02_22_033008_create_permissions_table.php',
+        'CreatePostsTable' => __DIR__ . '/../..' . '/database/migrations/2017_02_21_124315_create_posts_table.php',
+        'CreateRolesTable' => __DIR__ . '/../..' . '/database/migrations/2017_02_22_032957_create_roles_table.php',
         'CreateUsersTable' => __DIR__ . '/../..' . '/database/migrations/2014_10_12_000000_create_users_table.php',
         'DatabaseSeeder' => __DIR__ . '/../..' . '/database/seeds/DatabaseSeeder.php',
         'File_Iterator' => __DIR__ . '/..' . '/phpunit/php-file-iterator/src/Iterator.php',
